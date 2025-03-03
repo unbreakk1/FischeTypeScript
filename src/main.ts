2e0b24339545779f1b8429572ce6f2b52cbb1cc7
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-03-03 15:34:09 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+// Step 1:
+let age: number = 25
+
+// Step 2:
+for (let i = 1; i <= age; i++)
+    console.log(i)
+
+// Step 3:
+if (age > 18)
+    console.log('You are an adult.')
+else
+    console.log('You are not an adult.')
+
+// Step 4:
+let score: number = 0
+
+// Step 5:
+if (score !== 0)
+    console.log('Score is available.')
+
+// Step 6:
+if (score)
+    console.log('Score is evaluated as truthy.')
+else
+    console.log('Score is not evaluated as truthy.')
+
+// Step 7:
+let username: string = '';
+
+// Step 8:
+if (username)
+    console.log('Username is available.')
+
+// Step 9:
+if (!username)
+    console.log('Username is evaluated as falsy.')
+else
+    console.log('Username is not evaluated as falsy.')
+
+// Step 10:
+let isAdmin: boolean = false;
+
+// Step 11:
+if (isAdmin)
+    console.log('isAdmin is evaluated as truthy.')
+else
+    console.log('isAdmin is not evaluated as truthy.')
+
+// Step 12:
+if (!isAdmin)
+    console.log('isAdmin is false.')
+
+
